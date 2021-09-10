919f6bb887a1c774a1223006d7dadf4fdc9d01b4
hariom-ojha23 <hari_om.scsebtech@galgotiasuniversity.edu.in>
2021-09-10 19:34:15 +0530

    implemented home screen navbar and footer component

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../resource/products'
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <h2>product</h2>
+    </div>
+  )
+}
+
+export default ProductScreen
