d77426476f3181f78c55c2008b44092178001673
hariom-ojha23 <hari_om.scsebtech@galgotiasuniversity.edu.in>
2021-10-25 21:52:20 +0530

    created user profile screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ import generateToken from '../utils/generateToken.js'
 // @route   POST /api/users/
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  console.log(req.body)
   const { name, email, password } = req.body
   const userExist = await User.findOne({ email })
 
@@ -75,4 +74,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { loginUser, getUserProfile, registerUser }
+// @desc    Update users profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('user not found')
+  }
+})
+
+export { loginUser, getUserProfile, registerUser, updateUserProfile }
